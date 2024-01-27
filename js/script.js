a51a8dcbb87d71ff168b9e6835d820a32f36d20e
connorreidy1 <connorjreidy@gmail.com>
2024-01-27 11:31:06 -0700

    CORS issue fixed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
 console.log ('working')
 
-const apiKey = 'OPYT85RqlYZMUrcBDn4xvkSfa8bXHN2ITuEQpH1GLJQNw6D52mkB4joVSvBy'
-const button = document.querySelector('#submitButton')
 
-const getTeam = async () => {
-    const team = await axios.get(`https://api.sportmonks.com/football/v3?api_token=OPYT85RqlYZMUrcBDn4xvkSfa8bXHN2ITuEQpH1GLJQNw6D52mkB4joVSvBy`)
-}
+const baseURL = 'https://api.sportmonks.com/v3/football/'
+const apiToken = 'OPYT85RqlYZMUrcBDn4xvkSfa8bXHN2ITuEQpH1GLJQNw6D52mkB4joVSvBy'
+const button = document.querySelector('#submitButton')
+const leagueID = '501'
+
+
 
 button.addEventListener('click', async (event) => {
     event.preventDefault()
+    const leagueName = document.querySelector('#league-name')
+    const leagueLogo = document.querySelector('#league-logo')
+
     
+
+    const response = await axios.get(`${baseURL}leagues/${leagueID}?api_token=${apiToken}`);
+
+
+    const leagueData = response.data.data;
+
+
+    leagueName.innerText = leagueData.name;
+    leagueLogo.src = leagueData.image_path;
+
+
+console.log(response)
 })
