7c2cd5790bce0d2e5cfa1d578d433e64191de8ba
connorreidy1 <connorjreidy@gmail.com>
2024-01-28 11:14:37 -0700

    Font Page CSS

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # u1_project1_sports-api-app
 Unit 1 Project 1 Sports API App
+
+
+Hex codes
+Gold: #f9bc16 rgba(249,188,22,255)
+Navy Blue: #1c2d5a rgba(28,45,90,255)
+Gray Blue: #505e7e rgba(80,94,126,255)
+
+
+Resources
+
+Button CSS Help
+https://stackoverflow.com/questions/57547226/how-to-remove-background-from-button
--- js/script.js
+++ js/script.js
@@ -1,49 +1,46 @@
 console.log ('working')
 
-
-const baseURL = 'https://api.sportmonks.com/v3/football/'
-const apiToken = 'OPYT85RqlYZMUrcBDn4xvkSfa8bXHN2ITuEQpH1GLJQNw6D52mkB4joVSvBy'
-const button = document.querySelector('#submitButton')
-const leagueID = '501'
-
-
-
-button.addEventListener('click', async (event) => {
-    event.preventDefault()
-    const leagueName = document.querySelector('#league-name')
+document.addEventListener('DOMContentLoaded', async () => {
+    const baseURL = 'https://api.sportmonks.com/v3/football/'
+    const apiToken = 'OPYT85RqlYZMUrcBDn4xvkSfa8bXHN2ITuEQpH1GLJQNw6D52mkB4joVSvBy'
     const leagueLogo = document.querySelector('#league-logo')
-    const teamLogo = document.querySelector('#team-logo')
-    const teamName = document.querySelector('#team-name')
-    
-//League Name and Logo
-    // const response = await axios.get(`${baseURL}leagues/${leagueID}?api_token=${apiToken}`);
+    const teamsContainer = document.querySelector('#teams-container')
 
-    // const leagueData = response.data.data
-    // leagueName.innerText = leagueData.name
-    // leagueLogo.src = leagueData.image_path
+//League Logo
+    const leagueID = '501'
+    const responseLeague = await axios.get(`${baseURL}leagues/${leagueID}?api_token=${apiToken}`)
+    const leagueData = responseLeague.data.data
 
+    leagueLogo.src = leagueData.image_path
+
+//Team Logos and Names
     const teamIDs = ['273', '53', '284', '314', '66', '180', '258', '309', '62', '246', '734', '496' ]
 
-    
     for (const teamID of teamIDs) {
         const response = await axios.get(`${baseURL}teams/${teamID}?api_token=${apiToken}`);
         const teamData = response.data.data
 
         console.log(`Team Name: ${teamData.name}, Team Logo: ${teamData.image_path}`)
 
+        const teamSection = document.createElement('section')
+        teamSection.classList.add('team-container')
+
+        const teamLogo = document.createElement('img')
+        teamLogo.classList.add('team-logo')
+        teamLogo.alt = 'TeamLogo'
+        teamLogo.src = teamData.image_path
+
+        const teamName = document.createElement('h2')
+        teamName.classList.add('team-name')
+        teamName.innerText = teamData.name;
+
         teamLogo.src = teamData.image_path
         teamName.innerText = teamData.name
+
+        teamSection.appendChild(teamLogo)
+        teamSection.appendChild(teamName)
+
+        teamsContainer.appendChild(teamSection)
     }
-
-    
-
-    // const response = await axios.get(`${baseURL}teams/${aberdeenID}?api_token=${apiToken}`);
-
-    // const teamData = response.data.data
-    // teamLogo.src = teamData.image_path
-    // teamName.innerText = teamData.name
-
-    
-
-// console.log(response)
+ 
 })
